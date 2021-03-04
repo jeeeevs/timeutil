a00905789ab29051ef799d6dc2ac8188e2b8db92
Jeevan N <jeevan.nazar@publicissapient.com>
2021-03-04 22:41:28 +0000

    init2

--- timeutil.go
+++ timeutil.go
@@ -4,8 +4,12 @@ import "time"
 
 //UpTime for which the service was up
 func UpTime() func() int64 {
-	var startTime int64 = time.Now().Unix()
+	var startTime int64 = timeStamp()
 	return func() int64 {
-		return time.Now().Unix() - startTime
+		return timeStamp() - startTime
 	}
+}
+
+func timeStamp() {
+	return time.Now().Unix()
 }
